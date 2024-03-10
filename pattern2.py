f200c532f3ed4e115bdcad95a688373b71f40594
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-03-10 14:56:52 +0530

    pattern

---
+++ pattern2.py
@@ -0,0 +1,13 @@
+n = int(input("enter n: "))
+for i in range(n):
+    for j in range(i,n):
+        print("*",end = " ")
+    print()
+
+
+
+# n = int(input("Enter n: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end = " ")
+    print()
