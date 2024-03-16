c2a5cf2e82e746cb1c27ed8815e40b4b2eb55dd8
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-03-17 01:06:49 +0530

    function

---
+++ helloName.py
@@ -0,0 +1,5 @@
+def call_Name(first,last):
+    print(f"Hello {first} {last}! You just delved into python.")
+firstNam = input("Enter your first name: ")
+lastNam =input("Enter your last name: ")
+call_Name(firstNam,lastNam)
