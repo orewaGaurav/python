99069ed4f02b197ffe472b061eec486e154c0813
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-03-18 00:23:54 +0530

    RunnerUp

---
+++ runnerUp.py
@@ -0,0 +1,10 @@
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = list(map(int, input().split()))
+#     arr1 = set(arr)
+#     arr2 = sorted(arr)
+#     print(arr2[-2])
+
+n = int(input("enter no. of participates: "))
+arr = list(map(int,input("enter scores: ").split()))
+print(arr)
